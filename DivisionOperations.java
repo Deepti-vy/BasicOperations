755be9df0d7b323cfdecc12586de29073ce5b373
deepti <deeptiyatagiri@gmail.com>
2025-05-06 11:21:25 +0530

    save

---
+++ DivisionOperations.java
@@ -0,0 +1,21 @@
+package BasicOperations;
+
+public class DivisionOperations {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 20;
+
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        int divison = a / b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Sum (a + b) = " + sum);
+        System.out.println("Difference (a - b) = " + diff);
+        System.out.println("Product (a * b) = " + prod);
+        System.out.println("divison (a/ b) = " + divison);
+
+    }
+}
